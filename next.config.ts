c6105927f6b290a68ef11fc07b323e54be1405ae
Dennis A. Boanini <dennis.boanini01@gmail.com>
2025-11-30 20:16:43 +0100

    No cache for list page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    headers: () => [
+        {
+            source: "/",
+            headers: [
+                {
+                    key: "Cache-Control",
+                    value: "no-store",
+                },
+            ],
+        },
+    ],
 };
 
 export default nextConfig;
